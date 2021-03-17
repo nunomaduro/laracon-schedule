a225b911d3b3f275e1b21597d023dff415a8f505
Sascha Presnac <sascha.presnac@protonmail.com>
2021-03-17 13:40:43 +0100

    🐞 fixing path for windows

--- config/filesystems.php
+++ config/filesystems.php
@@ -7,5 +7,9 @@ return [
             'driver' => 'local',
             'root' => getcwd(),
         ],
+        'windowsconfig' => [
+            'driver' => 'local',
+            'root' => storage_path('../app/Commands/data'),
+        ],
     ],
 ];
