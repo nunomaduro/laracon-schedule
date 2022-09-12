ca8b3018e6ae44c3bf380f0f2a03c411e9f2eb9d
Nuno Maduro <enunomaduro@gmail.com>
2022-09-12 10:34:08 +0100

    Release v1.8.0

--- app/Commands/SchedulingCommand.php
+++ app/Commands/SchedulingCommand.php
@@ -2,24 +2,35 @@
 
 namespace App\Commands;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 
 class SchedulingCommand extends Command
 {
     public const BREAK_LENGTH_MINUTES = 20;
+
     public const TALK_LENGTH_MINUTES = 40;
+
     public const BREAK_SLOT_NAME = 'Break';
+
     public const EXIT_SLOT_NAME = 'Exit';
+
     public const LIGHTNING_TALKS_1 = 'LIGHTNING TALKS #1';
+
     public const LIGHTNING_TALKS_2 = 'LIGHTNING TALKS #2';
+
     public const TITLE = 'LARACON ONLINE SUMMER 2022';
+
     public const TIMEZONE = 'UTC';
+
     public const DATE = '2022-09-14';
+
     public const STARTS_AT_TIME = '12:45';
+
     public const ENDS_AT_TIME = '21:45';
+
     public const INDENT = '    ';
 
     /**
@@ -42,19 +53,19 @@ class SchedulingCommand extends Command
      * @var array<string, string>
      */
     protected $scheduling = [
-        '13:00' => '"Not Quite My Type" by Kai Sassnowski',
-        '13:40' => '"Kubernetes and Laravel" by Bosun Egberinde',
-        '14:20' => '"The future of Livewire" by Caleb Porzio',
+        '13:00' => '<options=bold>"Not Quite My Type"</> by Kai Sassnowski',
+        '13:40' => '<options=bold>"Kubernetes and Laravel"</> by Bosun Egberinde',
+        '14:20' => '<options=bold>"The future of Livewire"</> by Caleb Porzio',
         '15:00' => self::BREAK_SLOT_NAME,
         '15:20' => self::LIGHTNING_TALKS_1,
-        '16:35' => '"Abstracting too Early" by Matt Stauffer',
+        '16:35' => '<options=bold>"Abstracting too Early"</> by Matt Stauffer',
         '17:15' => self::BREAK_SLOT_NAME,
-        '17:35' => '"Laravel Update" by Taylor Otwell',
-        '18:35' => '"Database Performance for Application Developers" by Aaro Francis',
-        '19:15' => '"Christoph Dreams of Simple Code" by Christoph Rumpel',
+        '17:35' => '<options=bold>"Laravel Update"</> by Taylor Otwell',
+        '18:35' => '<options=bold>"Database Performance for Application Developers"</> by Aaro Francis',
+        '19:15' => '<options=bold>"Christoph Dreams of Simple Code"</> by Christoph Rumpel',
         '19:55' => self::BREAK_SLOT_NAME,
         '20:15' => self::LIGHTNING_TALKS_2,
-        '21:45' => '"Browsers are Magical Creatures" by Stephen Rees-Carter',
+        '21:45' => '<options=bold>"Browsers are Magical Creatures"</> by Stephen Rees-Carter',
     ];
 
     /**
@@ -63,20 +74,20 @@ class SchedulingCommand extends Command
      * @var array<int, string>
      */
     protected $lightningTalks1 = [
-        '"Sustainable Self-Care" by Marje Holmstrom-Sabo',
-        '"Let\'s Get Physical: Database Internals and You" by Tim Martin',
-        '"Deep Dive into Carbon" by Ralph J. Smit',
-        '"UI and Component testing with Cypress" by Marcel Pociot',
-        '"The Hitchhiker\'s Guide to the Laravel Community" by Caneco'
+        '<options=bold>"Sustainable Self-Care"</> by Marje Holmstrom-Sabo',
+        '<options=bold>"Let\'s Get Physical: Database Internals and You"</> by Tim Martin',
+        '<options=bold>"Deep Dive into Carbon"</> by Ralph J. Smit',
+        '<options=bold>"UI and Component testing with Cypress"</> by Marcel Pociot',
+        '<options=bold>"The Hitchhiker\'s Guide to the Laravel Community"</> by Caneco',
     ];
 
     protected $lightningTalks2 = [
-        '"Is there any problem Git interactive rebase can\'t solve?" by Rissa Jackson',
-        '"Meaningful Mentorship" by Alex Six',
-        '"I shall say... err define this only once" by Freek Van der Herten',
-        '"I can\'t believe it\'s not local!" by Chris Fidao',
-        '"Valid Variants of Validating Validation" by Luke Downing',
-        '"A Grab Bag of Useful Tips" by Colin DeCarlo'
+        '<options=bold>"Is there any problem Git interactive rebase can\'t solve?"</> by Rissa Jackson',
+        '<options=bold>"Meaningful Mentorship"</> by Alex Six',
+        '<options=bold>"I shall say... err define this only once"</> by Freek Van der Herten',
+        '<options=bold>"I can\'t believe it\'s not local!"</> by Chris Fidao',
+        '<options=bold>"Valid Variants of Validating Validation"</> by Luke Downing',
+        '<options=bold>"A Grab Bag of Useful Tips"</> by Colin DeCarlo',
     ];
 
     /**
@@ -85,8 +96,8 @@ class SchedulingCommand extends Command
      * @var array<string, string>
      */
     protected $community = [
-        'Telegram' => 'https://t.me/laracononline2021.',
-        'Discord' => 'https://discord.com/invite/mPZNm7A.'
+        'Telegram' => 'https://t.me/+J0X0cCt2z8c5YTEy',
+        'Discord' => 'https://discord.com/invite/mPZNm7A',
     ];
 
     /**
@@ -100,16 +111,16 @@ class SchedulingCommand extends Command
         $late = (int) $this->option('late');
 
         $this->line('');
-        $this->line(self::INDENT . '<options=bold,reverse;fg=magenta>' . self::TITLE . '</>');
+        $this->line(self::INDENT.'<options=bold,reverse;bg=white;fg=bright-red> '.self::TITLE.' </>');
         $this->line('');
 
-        $this->line(self::INDENT . 'Your timezone: ' . $userTimeZone . '.');
-        if ($late <> 0) {
-            $this->line(self::INDENT . 'Laracon is running ' . $late .' minutes late.');
+        $this->line(self::INDENT.'Your timezone: '.$userTimeZone.'.');
+        if ($late != 0) {
+            $this->line(self::INDENT.'Laracon is running '.$late.' minutes late.');
         }
 
-        $startsAt = self::DATE . ' ' . self::STARTS_AT_TIME;
-        $endsAt = self::DATE . ' ' . self::ENDS_AT_TIME;
+        $startsAt = self::DATE.' '.self::STARTS_AT_TIME;
+        $endsAt = self::DATE.' '.self::ENDS_AT_TIME;
 
         $hoursLeft = Carbon::parse($startsAt, self::TIMEZONE)
                 ->setTimezone($userTimeZone)
@@ -123,21 +134,21 @@ class SchedulingCommand extends Command
 
         if ($hoursLeft < 0) {
             $hoursLeft = abs($hoursLeft);
-            $this->line(self::INDENT . "Event status : Starts in $hoursLeft hours.");
+            $this->line(self::INDENT."Event status : Starts in $hoursLeft hours.");
         } elseif ($minutesLeft < 0) {
             $minutesLeft = abs($minutesLeft);
-            $this->line(self::INDENT . "Event status : Starts in $minutesLeft minutes.");
+            $this->line(self::INDENT."Event status : Starts in $minutesLeft minutes.");
         } elseif (Carbon::parse($endsAt, self::TIMEZONE)->setTimezone($userTimeZone)->isPast()) {
-            $this->line(self::INDENT . "Event status : Event has ended. See you next time!");
+            $this->line(self::INDENT.'Event status : Event has ended. See you next time!');
         } else {
-            $this->line(self::INDENT . "Event status : Already started.");
+            $this->line(self::INDENT.'Event status : Already started.');
         }
 
         $showedHappeningNowOnce = false;
 
         $this->line('');
         collect($this->scheduling)->each(function ($talk, $schedule) use ($userTimeZone, $late, &$showedHappeningNowOnce) {
-            $dateTime = Carbon::parse(self::DATE . " $schedule:00", self::TIMEZONE)
+            $dateTime = Carbon::parse(self::DATE." $schedule:00", self::TIMEZONE)
                 ->addMinutes($late)
                 ->setTimezone($userTimeZone);
 
@@ -148,20 +159,20 @@ class SchedulingCommand extends Command
                 $showedHappeningNowOnce = true;
             }
 
-            $this->line(self::INDENT . "<options={$lineOptions}>{$dateTime->calendar()}</> - $talk");
+            $this->line(self::INDENT."<options={$lineOptions}>{$dateTime->calendar()}</> - $talk");
 
             if ($talk === self::LIGHTNING_TALKS_1) {
-                collect($this->lightningTalks1)->each(fn($talk) => $this->line(self::INDENT . "  - {$talk}"));
+                collect($this->lightningTalks1)->each(fn ($talk) => $this->line(self::INDENT."  - {$talk}"));
             }
             if ($talk === self::LIGHTNING_TALKS_2) {
-                collect($this->lightningTalks2)->each(fn($talk) => $this->line(self::INDENT . "  - {$talk}"));
+                collect($this->lightningTalks2)->each(fn ($talk) => $this->line(self::INDENT."  - {$talk}"));
             }
         });
 
         $this->line('');
-        $this->line(self::INDENT . '<fg=magenta;options=bold>Join the community:</> ');
-        foreach($this->community as $platform => $link){
-            $this->line(self::INDENT . "$platform: $link");
+        $this->line(self::INDENT.'<fg=magenta;options=bold>Join the community:</> ');
+        foreach ($this->community as $platform => $link) {
+            $this->line(self::INDENT."$platform: $link");
         }
         $this->line('');
     }
@@ -218,12 +229,12 @@ class SchedulingCommand extends Command
      *  `./data/windowsZones.json` file content from windowsZones.xml
      *  https://github.com/unicode-org/cldr/blob/master/common/supplemental/windowsZones.xml
      *
-     * @param string $timeZoneId Windows time zone identifier (i.e. 'E. South America Standard Time')
+     * @param  string  $timeZoneId Windows time zone identifier (i.e. 'E. South America Standard Time')
      * @return string
      */
     private function getIanaTimeZoneFromWindowsIdentifier($timeZoneId)
     {
-        $json =Storage::disk('windowsconfig')->get('windowsZones.json');
+        $json = Storage::disk('windowsconfig')->get('windowsZones.json');
         $zones = collect(json_decode($json));
 
         $timeZone = $zones->firstWhere('windowsIdentifier', '=', $timeZoneId);
